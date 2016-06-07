a3bdebf3a4a76c2eee3bf38e1143bd22a0d2b789
Dustin Wind <dusty@posteo.de>
2016-06-07 13:12:44 +0200

    added first assert which checks whether there is a int-overflow or a item is not within a tvb-boundary

--- src/plugins/doip/doip-helper.c
+++ src/plugins/doip/doip-helper.c
@@ -4,9 +4,13 @@
 #include "doip-helper.h"
 
 #include "config.h"
+#include <assert.h>
 #include <epan/packet.h>
 #include <epan/proto.h>
 #include <epan/tvbuff.h>
+
+static void
+item_is_in_tvb_range(const gint pos, const gint length, tvbuff_t *tvb);
 
 gboolean
 insert_item_to_tree(proto_tree *tree, const gint hf, tvbuff_t *tvb, gint rel_pos, gint length, const guint enc)
@@ -14,6 +18,8 @@ insert_item_to_tree(proto_tree *tree, const gint hf, tvbuff_t *tvb, gint rel_pos
     gint abs_pos;
     gboolean overflow;
     gboolean error;
+
+    item_is_in_tvb_range(rel_pos, length, tvb);
 
     abs_pos = payload_offset_to_abs_offset(rel_pos);
         
@@ -27,3 +33,25 @@ insert_item_to_tree(proto_tree *tree, const gint hf, tvbuff_t *tvb, gint rel_pos
     }
     return error;
 }
+
+static void
+item_is_in_tvb_range(const gint pos, const gint length, tvbuff_t *tvb)
+{
+    guint tvb_len = tvb_reported_length(tvb);
+    gint end_pos = pos + length;
+
+    /* Check for an int overflow */
+    assert(end_pos > pos);
+
+    /* check whether the item is within a given tvb-boundary */
+    assert(end_pos < (gint) tvb_len);
+}
+
+
+
+
+
+
+
+
+
