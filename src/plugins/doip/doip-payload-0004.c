1262f6bb44a54f7873d02966b47952f2c0724f93
Dustin Wind <dusty@posteo.de>
2016-06-08 08:11:56 +0200

    adapted source file to coding guidlines (4 spaces instead of tab) (https://www.wireshark.org/tools/modelines.html)

--- src/plugins/doip/doip-payload-0004.c
+++ src/plugins/doip/doip-payload-0004.c
@@ -52,44 +52,44 @@ static const gchar *description = "Vehicle announcement message / vehicle identi
  * on table 20
 */
 static const range_string further_action_values[] = {
-	{ 0x00, 0x00, "No further action required" },
-	{ 0x01, 0x0F, "Reserved by this part of ISO 13400" },
-	{ 0x10, 0x10, "Routing activation required to initiate central security." },
-	{ 0x11, 0xFF, "Available for additional OEM-specific use." },
-	{ 0x00, 0x00, NULL}
+    { 0x00, 0x00, "No further action required" },
+    { 0x01, 0x0F, "Reserved by this part of ISO 13400" },
+    { 0x10, 0x10, "Routing activation required to initiate central security." },
+    { 0x11, 0xFF, "Available for additional OEM-specific use." },
+    { 0x00, 0x00, NULL}
 };
 
 /** Values are defined in ISO 13400-2:2012(E)
 * on table 21
 */
 static const range_string vin_gid_sync_values[] = {
-	{ 0x00, 0x00, "VIN and/or GID are synchronized" },
-	{ 0x01, 0x0F, "Reserved by this part of ISO 13400" },
-	{ 0x10, 0x10, "Incomplete: VIN and GID are NOT synchronized." },
-	{ 0x11, 0xFF, "Reserved by this part of ISO 13400" },
-	{ 0x00, 0x00, NULL }
+    { 0x00, 0x00, "VIN and/or GID are synchronized" },
+    { 0x01, 0x0F, "Reserved by this part of ISO 13400" },
+    { 0x10, 0x10, "Incomplete: VIN and GID are NOT synchronized." },
+    { 0x11, 0xFF, "Reserved by this part of ISO 13400" },
+    { 0x00, 0x00, NULL }
 };
 
 /** Values are defined in ISO 13400-2:2012(E)
 * on table 39
 */
 static const range_string log_addr_values[] = {
-	{ 0x0000, 0x0000, "ISO/SAE reserved" },
-	{ 0x0001, 0x0DFF, "Vehicle manufacturer specific" },
-	{ 0x0E00, 0x0FFF, "Reserved for addresses of external test equipment" },
-	{ 0x0E00, 0x0E7F, "External legislated diagnostics test equipment (e.g. for emissions test scan-tool use)" },
-	{ 0x0E80, 0x0EFF, "External vehicle-manufacturer-/aftermarket-enhanced diagnostics test equipment" },
-	{ 0x0F00, 0x0F7F, "Internal data collection/on-board diagnostic equipment (for vehicle-manufacturer use only)" },
-	{ 0x0F80, 0x0FFF, "External prolonged data collection equipment (vehicle data recorders and loggers, e.g. used by insurance companies or to collect vehicle fleet data)" },
-	{ 0x1000, 0x7FFF, "Vehicle manufacturer specific" },
-	{ 0x8000, 0xCFFF, "ISO/SAE reserved" },
-	{ 0xD000, 0xDFFF, "Reserved for SAE Truck & Bus Control and Communication Committee" },
-	{ 0xE000, 0xE3FF, "ISO/SAE-reserved functional group addresses" },
-	{ 0xE000, 0xE000, "ISO 27145 WWH-OBD functional group address" },
-	{ 0xE001, 0xE3FF, "ISO/SAE reserved" },
-	{ 0xE400, 0xEFFF, "Vehicle-manufacturer-defined functional group logical addresses" },
-	{ 0xF000, 0xFFFF, "ISO/SAE reserved" },
-	{ 0x0000, 0x0000, NULL }
+    { 0x0000, 0x0000, "ISO/SAE reserved" },
+    { 0x0001, 0x0DFF, "Vehicle manufacturer specific" },
+    { 0x0E00, 0x0FFF, "Reserved for addresses of external test equipment" },
+    { 0x0E00, 0x0E7F, "External legislated diagnostics test equipment (e.g. for emissions test scan-tool use)" },
+    { 0x0E80, 0x0EFF, "External vehicle-manufacturer-/aftermarket-enhanced diagnostics test equipment" },
+    { 0x0F00, 0x0F7F, "Internal data collection/on-board diagnostic equipment (for vehicle-manufacturer use only)" },
+    { 0x0F80, 0x0FFF, "External prolonged data collection equipment (vehicle data recorders and loggers, e.g. used by insurance companies or to collect vehicle fleet data)" },
+    { 0x1000, 0x7FFF, "Vehicle manufacturer specific" },
+    { 0x8000, 0xCFFF, "ISO/SAE reserved" },
+    { 0xD000, 0xDFFF, "Reserved for SAE Truck & Bus Control and Communication Committee" },
+    { 0xE000, 0xE3FF, "ISO/SAE-reserved functional group addresses" },
+    { 0xE000, 0xE000, "ISO 27145 WWH-OBD functional group address" },
+    { 0xE001, 0xE3FF, "ISO/SAE reserved" },
+    { 0xE400, 0xEFFF, "Vehicle-manufacturer-defined functional group logical addresses" },
+    { 0xF000, 0xFFFF, "ISO/SAE reserved" },
+    { 0x0000, 0x0000, NULL }
 };
 
 /* values which will be displayed for payload type 0004 in proto_tree */
@@ -112,32 +112,32 @@ register_proto_doip_payload_0004(gint proto_doip)
                 HFILL
             }
         },
-		{
-			&hf_log_addr,
-			{
-				"Logical Address",
-				"doip.payload.la",
-				FT_UINT16,
-				BASE_HEX,
-				log_addr_values,
-				0x0,
-				"The logical address that is assigned to the responding DoIP entity. It can be used, for example, to address diagnostic requests directly to the DoIP entity.",
-				HFILL
-			}
-		},
-		{
-			&hf_eid,
-			{
-				"Entity identification",
-				"doip.payload.eid",
-				FT_ETHER,
-				BASE_NONE,
-				NULL,
-				0x0,
-				"The unique identification of the DoIP entities in order to separate their responses even before the VIN is programmed to or recognized by the DoIP devices (e.g. during the vehicle assembly process).",
-				HFILL
-			}
-		},
+        {
+            &hf_log_addr,
+            {
+                "Logical Address",
+                "doip.payload.la",
+                FT_UINT16,
+                BASE_HEX,
+                log_addr_values,
+                0x0,
+                "The logical address that is assigned to the responding DoIP entity. It can be used, for example, to address diagnostic requests directly to the DoIP entity.",
+                HFILL
+            }
+        },
+        {
+            &hf_eid,
+            {
+                "Entity identification",
+                "doip.payload.eid",
+                FT_ETHER,
+                BASE_NONE,
+                NULL,
+                0x0,
+                "The unique identification of the DoIP entities in order to separate their responses even before the VIN is programmed to or recognized by the DoIP devices (e.g. during the vehicle assembly process).",
+                HFILL
+            }
+        },
         {
             &hf_gid,
             {
@@ -152,26 +152,26 @@ register_proto_doip_payload_0004(gint proto_doip)
             }
         },
         {
-			&hf_further_action_req,
+            &hf_further_action_req,
             {
                 "Further action required",
                 "doip.payload.far",
-				FT_UINT8,
-				BASE_HEX,
-				further_action_values,
+                FT_UINT8,
+                BASE_HEX,
+                further_action_values,
                 0x0,
                 "The additional information to notify the external test equipment that there are either DoIP entities with no initial connectivity or that a centralized security approach is used.",
                 HFILL
             }
         },
         {
-			&hf_vin_gid_sync,
+            &hf_vin_gid_sync,
             {
                 "VIN/GID sync. status",
                 "doip.payload.vgss",
-				FT_UINT8,
-				BASE_HEX,
-				vin_gid_sync_values,
+                FT_UINT8,
+                BASE_HEX,
+                vin_gid_sync_values,
                 0x0,
                 "The additional information to notify the external test equipment that all DoIP entities have synchronized their information about the VIN or GID of the vehicle.",
                 HFILL
@@ -182,10 +182,10 @@ register_proto_doip_payload_0004(gint proto_doip)
 
     static gint *ett[] =
     {
-		&ett_vehicle_announce_id_msg
+        &ett_vehicle_announce_id_msg
     };
 
-	/* one-time registration after Wireshark is started */
+    /* one-time registration after Wireshark is started */
     proto_register_field_array(proto_doip, hf, array_length(hf));
     proto_register_subtree_array(ett, array_length(ett));
 }
@@ -196,10 +196,10 @@ dissect_payload_0004(doip_header *header, proto_item *pitem, packet_info *pinfo)
 {
     tvbuff_t *tvb;
     proto_tree *doip_tree;
-	guint32 payloadLength;
+    guint32 payloadLength;
 
-	/* get the length of the payload */
-	payloadLength = header->payload.length;
+    /* get the length of the payload */
+    payloadLength = header->payload.length;
 
     /* set info column to description */
     col_set_str(pinfo->cinfo, COL_INFO, description);
@@ -207,7 +207,7 @@ dissect_payload_0004(doip_header *header, proto_item *pitem, packet_info *pinfo)
     tvb = retrieve_tvbuff(header);
 
     /* attach a new tree to proto_item pitem */
-	doip_tree = proto_item_add_subtree(pitem, ett_vehicle_announce_id_msg);
+    doip_tree = proto_item_add_subtree(pitem, ett_vehicle_announce_id_msg);
 
     /* check for a valid tvbuff_t */
     if(doip_tree && tvb)
@@ -231,30 +231,30 @@ fill_tree(proto_tree *tree, tvbuff_t *tvb, guint32 payloadLength)
     const gint VIN_LEN = 17;
 
     const gint REL_LOG_ADDR_POS = 17;
-	const gint LOG_ADDR_LEN = 2;
+    const gint LOG_ADDR_LEN = 2;
 
-	const gint REL_EID_POS = 19;
-	const gint EID_LEN = 6;
+    const gint REL_EID_POS = 19;
+    const gint EID_LEN = 6;
 
-	const gint REL_GID_POS = 25;
-	const gint GID_LEN = 6;
+    const gint REL_GID_POS = 25;
+    const gint GID_LEN = 6;
 
-	const gint REL_FURTHER_ACTION_REQ_POS = 31;
-	const gint FURTHER_ACTION_REQ_LEN = 1;
+    const gint REL_FURTHER_ACTION_REQ_POS = 31;
+    const gint FURTHER_ACTION_REQ_LEN = 1;
 
-	const gint REL_VIN_GID_SYNC = 32;
-	const gint VIN_GID_SYNC_LEN = 1;
+    const gint REL_VIN_GID_SYNC = 32;
+    const gint VIN_GID_SYNC_LEN = 1;
 
     gboolean vin_gid_sync_is_present = ((gint) payloadLength) >= (REL_VIN_GID_SYNC + VIN_GID_SYNC_LEN);
 
 
-	insert_item_to_tree(tree, hf_vin, tvb, REL_VIN_POS, VIN_LEN, ENC_ASCII);
-	insert_item_to_tree(tree, hf_log_addr, tvb, REL_LOG_ADDR_POS, LOG_ADDR_LEN, ENC_BIG_ENDIAN);
-	insert_item_to_tree(tree, hf_eid, tvb, REL_EID_POS, EID_LEN, ENC_NA);
-	insert_item_to_tree(tree, hf_gid, tvb, REL_GID_POS, GID_LEN, ENC_NA);
-	insert_item_to_tree(tree, hf_further_action_req, tvb, REL_FURTHER_ACTION_REQ_POS, FURTHER_ACTION_REQ_LEN, ENC_BIG_ENDIAN);
+    insert_item_to_tree(tree, hf_vin, tvb, REL_VIN_POS, VIN_LEN, ENC_ASCII);
+    insert_item_to_tree(tree, hf_log_addr, tvb, REL_LOG_ADDR_POS, LOG_ADDR_LEN, ENC_BIG_ENDIAN);
+    insert_item_to_tree(tree, hf_eid, tvb, REL_EID_POS, EID_LEN, ENC_NA);
+    insert_item_to_tree(tree, hf_gid, tvb, REL_GID_POS, GID_LEN, ENC_NA);
+    insert_item_to_tree(tree, hf_further_action_req, tvb, REL_FURTHER_ACTION_REQ_POS, FURTHER_ACTION_REQ_LEN, ENC_BIG_ENDIAN);
     /* only insert this item if needed, since it is optional */
-	if (vin_gid_sync_is_present)
+    if (vin_gid_sync_is_present)
     {
         insert_item_to_tree(tree, hf_vin_gid_sync, tvb, REL_VIN_GID_SYNC, VIN_GID_SYNC_LEN, ENC_BIG_ENDIAN);
     }
