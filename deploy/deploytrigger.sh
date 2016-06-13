52a5f1e3a51bc70e1e04368c129701ab64ef3319
Dustin Wind <dusty@posteo.de>
2016-06-14 00:21:39 +0200

    deploytrigger.sh may now trigger a real deploy [TDEPLOY]

--- deploy/deploytrigger.sh
+++ deploy/deploytrigger.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+###
+# Copyright 2017 The Open Source Research Group,
+#                University of Erlangen-Nürnberg
+#
+# Licensed under the GNU AFFERO GENERAL PUBLIC LICENSE, Version 3.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.gnu.org/licenses/gpl.html
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 set -ue
 
 AMOSHASH=`git log -n 1 --pretty=format:%H`
@@ -10,16 +27,19 @@ if [[ $AMOSTAG == *"release"* ]]
 then
     if [[ $AMOSTAG == *"candidate"* ]]
     then
-        echo 'release candidate';
+        echo 'deploying test--candidate';
+        bash deploy/testdeploy.sh
     else
-        echo 'release';
+        echo 'deploying realease--candidate';
+        bash deploy/releasedeploy.sh
     fi
 else
     if [[ $COMMITMSG == *"TDEPLOY"* ]]
     then
-        echo 'test release'
+        echo 'deploying test--candidate';
+        bash deploy/testdeploy.sh
     else
-        echo 'no release';
+        echo 'not deploying anything';
     fi
         
 fi
