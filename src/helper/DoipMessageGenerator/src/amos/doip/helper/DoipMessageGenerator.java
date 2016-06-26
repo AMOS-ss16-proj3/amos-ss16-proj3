21bffddb4a25d6ebe0552efe1ca1554370396171
Dustin Wind <dusty@posteo.de>
2016-06-26 12:53:31 +0200

    implemented some more payload types; issue #9

--- src/helper/DoipMessageGenerator/src/amos/doip/helper/DoipMessageGenerator.java
+++ src/helper/DoipMessageGenerator/src/amos/doip/helper/DoipMessageGenerator.java
@@ -2,21 +2,48 @@ package amos.doip.helper;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.nio.charset.Charset;
 
 public class DoipMessageGenerator {
+
+    public static final Charset ASCII = Charset.forName("US-ASCII");
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 
-        DoipMessage dmsg1 = new DoipMessage(1, new byte[]{0x04});
-        DoipMessage dmsg2 = new DoipMessage(0x8001, new byte[]{0x03, (byte)0x80, (byte)0xe4,0x00, 0x3e, (byte)0x80});
+        DoipMessage[] dmsgs = {
+
+            /* 0 - Generic doip header negative acknowledge */
+            new DoipMessage(0, new byte[]{0}),
+            new DoipMessage(0, new byte[]{1}),
+            new DoipMessage(0, new byte[]{2}),
+            new DoipMessage(0, new byte[]{3}),
+            new DoipMessage(0, new byte[]{4}),
+            new DoipMessage(0, new byte[]{5}),
+            new DoipMessage(0, new byte[]{(byte)0xFF}),
+
+
+            /* 0x0001 - Vehicle identification request message */
+            new DoipMessage(1, new byte[]{0x0}),
+
+            /* 0x0002 - vehice identification request message with EID */
+            new DoipMessage(2, new byte[]{0x00, 0x11, 0x22, 0x33, 0x44, 0x55}),
+
+            /* 0x0003 - vehicle identification request message with VIN */
+            new DoipMessage(3, "Hallo Welt!".getBytes(ASCII)),
+            new DoipMessage(3, "12345678901234567".getBytes(ASCII)),
+
+
+
+            /* 0x8001 - */
+            new DoipMessage(0x8001, new byte[]{0x03, (byte)0x80, (byte)0xe4,0x00, 0x3e, (byte)0x80})
+        };
         
-        byte[] msg1 = dmsg1.toByteArray();
-        byte[] msg2 = dmsg2.toByteArray();
         
         List<byte[]> msgs = new LinkedList<byte[]>();
-        //msgs.add(msg1);
-        msgs.add(msg2);
+        for(DoipMessage dmsg : dmsgs){
+            msgs.add(dmsg.toByteArray());
+        }
         
         
         Thread server = new Thread(new DoipServer());
@@ -32,8 +59,5 @@ public class DoipMessageGenerator {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        
-        
     }
-
 }
