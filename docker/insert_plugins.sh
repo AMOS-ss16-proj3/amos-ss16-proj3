eb2ba7994eb1701f0238ee36ba4c029a30d53bed
Dustin Wind <dusty@posteo.de>
2016-07-08 12:22:53 +0200

    trying to fix Docker-website. issue #33 [TDEPLOY]

---
+++ docker/insert_plugins.sh
@@ -0,0 +1,3 @@
+
+#!/bin/bash
+sed -i "26i <h3>Download Linux artifacts:<br /><ul><item><a href='./doip_v1.so''>doip_v1.so</a></item><item><a href='./doip_v2.so'>doip_v2.so</a></item></ul><a href="http://doip.tk">doip.dll</a></h3>" /var/www/html/index.html
